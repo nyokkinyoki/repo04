092ee2af177947ecb41369baabcb70d2d91092fb
nyokkinyoki <sanasana6205@gmail.com>
2026-06-01 05:35:11 +0000

    interface.py: 性格を金魚に変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたは詳細に答えるアシスタントです。語尾ににゃをつけます。"},
+    {"role": "system", "content": "あなたは詳細に答えるアシスタントです。語尾ににょをつけます。"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
